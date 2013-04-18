066cde0b68f94b3dd87445faf5372b70a9685c45
MattLorenzo <matiaslorenzo.so@gmail.com>
2013-04-18 16:57:20 -0300

    cambios

--- prueba.c
+++ prueba.c
@@ -3,6 +3,7 @@
 int main (){
     printf("Hola,el archivo fue modificado!!\n");
     printf("Esta es una nueva linea de actualizacion\n");
+    printf("Incremento de potencial electrico\n");
 
     return 0;
 
